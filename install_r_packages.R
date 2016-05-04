a65abe014d92197e2a9e512e17af54ffd3ef9c23
Jolien Gay <gay.jolien@hotmail.com>
2016-05-04 21:16:56 +0200

    added install_r_packages.R

---
+++ install_r_packages.R
@@ -0,0 +1,31 @@
+# First things
+install.packages("RNeXML", repos = "http://cran.uk.r-project.org")
+
+# Second things
+install.packages("ape", repos = "http://cran.uk.r-project.org")
+install.packages("adephylo", repos = "http://cran.uk.r-project.org")
+install.packages("data.table", repos = "http://cran.uk.r-project.org")
+install.packages("devtools", repos = "http://cran.uk.r-project.org")
+install.packages("ggplot2", repos = "http://cran.uk.r-project.org")
+install.packages("gridExtra", repos = "http://cran.uk.r-project.org")
+install.packages("knitr", repos = "http://cran.uk.r-project.org")
+install.packages("lintr", repos = "http://cran.uk.r-project.org")
+install.packages("phangorn", repos = "http://cran.uk.r-project.org")
+install.packages("PBD", repos = "http://cran.uk.r-project.org")
+install.packages("rmarkdown", repos = "http://cran.uk.r-project.org")
+install.packages("testit", repos = "http://cran.uk.r-project.org")
+install.packages("testthat", repos = "http://cran.uk.r-project.org")
+install.packages("XML", repos = "http://cran.uk.r-project.org")
+install.packages("crayon", repos = "http://cran.uk.r-project.org")
+install.packages("httr", repos = "http://cran.uk.r-project.org")
+install.packages("jsonlite", repos = "http://cran.uk.r-project.org")
+install.packages("memoise", repos = "http://cran.uk.r-project.org")
+install.packages("rex", repos = "http://cran.uk.r-project.org")
+install.packages("withr", repos = "http://cran.uk.r-project.org")
+
+# Third things
+
+devtools::install_github("jimhester/covr")
+devtools::install_github("richelbilderbeek/beastscriptr")
+devtools::install_github("richelbilderbeek/rBEAST")
+devtools::install_github("richelbilderbeek/ribir")
